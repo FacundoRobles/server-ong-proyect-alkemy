de6587285481bafb1a67e0a9cbe24898be2ea25c
Matias <floresmatias0@gmail.com>
2021-09-28 02:12:37 -0300

    feat/OT76-41 create model,seeder and migration category

---
+++ seeders/20210928045130-category.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     await queryInterface.bulkInsert('Categories', [{
+      name: 'Transitory',
+      description: 'this is a description',
+      deletedAt: new Date,
+      createdAt: new Date,
+      updatedAt: new Date
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};

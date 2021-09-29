c0e08c35c0b9d83ca66b29d4094fbf02228786a8
MarZuleta <martinzuleta@hotmail.com>
2021-09-29 13:17:04 -0300

    Delete testimonial, fetchOne and fetchAll added

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -9,21 +9,72 @@ module.exports = {
                     message:'Please complete all required fields'
                 });
             }
-            const newTestimonial = testimonial.create(req.body);
-            const response = {
+            const newTestimonial = await testimonial.create(req.body);
+            return res.status(201).send({
                 success: true,
                 data: {
                     testimonials: newTestimonial
                 }
-            };
-            return res.status(201).send(response);
+            });
         } catch(err){
             console.log(err.message);
-            const response = {
+            return res.status(400).send({
                 success: false,
                 message: err.message
-            }
-            return res.status(400).send(response);
+            });
+        }
+    },
+    async deleteTestimonial(req, res) {
+        try {
+            const testimonialToDelete = await testimonial.fetchOne(req.params.id);
+            if(testimonialToDelete){
+                const deletedTestimonial = await testimonial.delete(testimonialToDelete);
+                return res.status(204).send({
+                    success: true,
+                    data: {
+                        testimonials: deletedTestimonial
+                    }
+                });
+            } else{
+            return res.status(404).send({
+                success: false,
+                message: 'Testimonial to delete not found'
+            });
+        }
+        } catch(err){
+            console.log(err.message);
+            return res.status(400).send({
+                success: false,
+                message: err.message
+            });
+
+        }
+    },
+    async fetchTestimonials(req, res){
+        try {
+            if(!req.params.id){
+            const allTestimonials = await testimonial.fetchAll();
+            return res.status(200).send({
+                success: true,
+                data: {
+                    testimonials: allTestimonials
+                }
+            });
+        } else {
+            const getTestimonial = await testimonial.fetchOne(req.params.id);
+            return res.status(200).send({
+                success: true,
+                data: {
+                    testimonials: getTestimonial
+                }
+            });
+        }
+        } catch (err){
+            console.log(err.message);
+            return res.status(400).send({
+                success: false,
+                message: err.message
+            });
         }
     }
 }
--- services/testimonialServices.js
+++ services/testimonialServices.js
@@ -1,7 +1,16 @@
-const testimonial = require('../models/testimonial').Testimonial;
+const testimonial = require('../models').Testimonial;
 
 module.exports = {
     async create(body) {
         return await testimonial.create(body);
+    },
+    async delete(testimonialToDelete) {
+        return await testimonialToDelete.destroy();
+    },
+    async fetchAll() {
+        return await testimonial.findAll();
+    },
+    async fetchOne(id){
+        return await testimonial.findByPk(id);
     }
 }

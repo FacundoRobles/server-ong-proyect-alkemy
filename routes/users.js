58e559043b9bd8ce41bf2a7b14d7009d8e644a15
Matias <floresmatias0@gmail.com>
2021-10-02 02:11:27 -0300

    feat/OT76-44, delete logical user

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const { getUsers } = require('../controllers/userController')
+const { getUsers,validateUserDb,logicalDeleteUser } = require('../controllers/userController');
+const { validationResult } = require('express-validator');
 
 router.get('/', (req, res, next) => {
 
@@ -14,6 +15,30 @@ router.get('/', (req, res, next) => {
       success: false,
       data: err
     }))
+});
+
+router.put('/:idUser', validateUserDb,(req, res, next) => {
+  const { idUser } = req.params;
+
+  const errors = validationResult(req);
+    
+  if (!errors.isEmpty()) {
+      return res.status(401).json({ errors: errors.array() });
+  }
+
+  logicalDeleteUser(idUser)
+  .then(updated => {
+      if(updated){
+          return res.status(201).send({
+              success: true,
+              data: `user ${idUser} erased`
+          })
+      };
+      res.status(401).send({
+          success: false,
+          data: `sorry dont finded user ${idUser}`
+      });
+  });
 });
 
 module.exports = router;

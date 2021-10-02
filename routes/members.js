8a18a43712393bff43a82b62ac0ad19aaff43207
sznitowski <vsznitowski@gmail,com>
2021-10-02 16:45:26 -0300

    feat/OT76-90 endpoint udapte member

--- routes/members.js
+++ routes/members.js
@@ -1,26 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
-const { getMembers,newMember } = require('../controllers/memberController');
+const { getMembers, newMember, updateOne } = require('../controllers/memberController');
 
 router.get('/', (req, res, next) => {
-     getMembers()
-    .then(members => res.status(201).json(members))
-    .catch(err => res.status(401).send(err.message))
+    getMembers()
+        .then(members => res.status(201).json(members))
+        .catch(err => res.status(401).send(err.message))
     //res.status(201).send('hola')
 });
 
 router.post('/', (req, res, next) => {
-    const { name,image } = req.body
+    const { name, image } = req.body
 
-    newMember(name,image)
-    .then(member => {
-        if(typeof(member) !== 'string'){
-            return res.status(201).json(member)
-        }
-
-        res.status(401).send(member)
-    })
+    newMember(name, image)
+        .then(member => {
+            if (typeof (member) !== 'string') {
+                return res.status(201).json(member)
+            }
+            res.status(401).send(member)
+        })
 });
+
+ router.put('/:idMember', (req, res, next) => {
+    const { idMember } = req.params
+    const { name, image } = req.body
+
+    updateOne(idMember, name, image)
+        .then(updated => {
+            console.log(updated)
+            if (updated) {
+                return res.status(201).json(`member ${idMember} modified`)
+            }})
+        .catch (err => res.status(401).send(`sorry dont finded member ${idMember}`)) 
+})
+
+
 
 module.exports = router;

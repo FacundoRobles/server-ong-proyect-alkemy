533b1e76ed70f96f5b7c728873953140a88fb660
Matias <floresmatias0@gmail.com>
2021-10-02 01:11:43 -0300

    feat/OT76-33 and feat/OT76-25, login user and jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,26 +17,33 @@ module.exports = {
             }
            return await createUser(newUserData)     
         })
-    }
-}
+    },
+    validationUserFields: [
+        body('email','must be a email').isEmail()
+        .custom(async(email) => {
+            await verifyEmail(email)
+            .then(user => {
+                if (user) {
+                    return Promise.reject('E-mail already in use');
+                }
+            });
+        }),
+        body('firstName','please insert a name').not().isEmpty(),
+        // body('image','please insert a image').not().isEmpty(), for the future 
+        body('lastName','please insert a last name').not().isEmpty(),
+        body('password','please insert a password')
+        .not().isEmpty()
+        .isLength({ min: 6 })
+        .withMessage('must be at least 6 chars long')
+        .matches(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{6,16}$/)
+        .withMessage('at least one digit, at least one lowercase, and at least one uppercase. It may have other symbols')
+    ],
+    validationLoginFields: [
+        body('password','please insert a password').not().isEmpty(),
+        body('email','must be a email').not().isEmpty()
+        .withMessage('not has be empty')
+        .isEmail()
+        .withMessage('must be a email')
+    ]
 
-module.exports.validationUserFields = [
-    body('email','must be a email').isEmail()
-    .custom(async(email) => {
-        await verifyEmail(email)
-        .then(user => {
-            if (user) {
-                return Promise.reject('E-mail already in use');
-            }
-        });
-    }),
-    body('firstName','please insert a name').not().isEmpty(),
-    // body('image','please insert a image').not().isEmpty(), for the future 
-    body('lastName','please insert a last name').not().isEmpty(),
-    body('password','please insert a password')
-    .not().isEmpty()
-    .isLength({ min: 6 })
-    .withMessage('must be at least 6 chars long')
-    .matches(/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{6,16}$/)
-    .withMessage('at least one digit, at least one lowercase, and at least one uppercase. It may have other symbols')
-]
+}
